65cf9a6d496c6490d7ed35020bdcc5788dd14178
Ironious4 <tyronekariuki@gmail.com>
2024-10-22 22:44:05 +0300

    changes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from your_app_name import app  # or however your Flask/Django app is named
+from app import app  # or however your Flask/Django app is named
 
 if __name__ == "__main__":
     app.run()
