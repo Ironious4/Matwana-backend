fdfdae69e853d4fa1b188fd552dc34b791400c3e
Ironious4 <tyronekariuki@gmail.com>
2024-10-23 12:26:03 +0300

    database

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ DATABASE = os.environ.get(
 app=Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = postgresql://matwana_database_user:QKfu63J4VfBIupUphoMfxfB2UdpM5TlI@dpg-csbvb29u0jms73eeutsg-a/matwana_database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY']='e173db52f146f6d5e957a922'
 app.config['JWT_ACCESS_TOKEN_EXPIRES']=timedelta(hours=2)
