4d3e25d63ae24f6cd08edfc52ae453932ec445ec
Ironious4 <tyronekariuki@gmail.com>
2024-10-27 11:19:37 +0300

    database

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ DATABASE = os.environ.get(
 app=Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://matwana_database_user:QKfu63J4VfBIupUphoMfxfB2UdpM5TlI@dpg-csbvb29u0jms73eeutsg-a/matwana_database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 app.config['FLASK_ENV'] = 'development'
